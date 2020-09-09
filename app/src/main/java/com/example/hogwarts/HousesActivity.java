8089a6d9baca4273499f7a55e6a9da9907352b00
Tererai <terrizun@gmail.com>
2020-09-09 15:13:55 +0200

    first commit

---
+++ app/src/main/java/com/example/hogwarts/HousesActivity.java
@@ -0,0 +1,14 @@
+package com.example.hogwarts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HousesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_houses);
+    }
+}
